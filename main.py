650f802b59c82f0d6cd30b2e0dd9f3a10d4a498c
lahl <domee.lahl@gmail.com>
2020-11-02 20:28:18 +0100

    Add example matrix

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 if __name__ == "__main__":
-    pass
+    example_points = [[0/5], [3/2], [5/10]]
+
+    # f(0) = a*0^2 + b*0 + c = 5
+    # f(3) = a*3^2 + b*3 + c = 2
+    # f(5) = a*5^2 + b*5 + c = 10
+
+    matrix = [
+        [example_points[0][0]**2, example_points[0][0], 1],
+        [example_points[1][0]**2, example_points[1][0], 1],
+        [example_points[2][0]**2, example_points[2][0], 1],
+        [example_points[0][0], example_points[1][0], example_points[2][0]]
+    ]
+
+    print(matrix)
