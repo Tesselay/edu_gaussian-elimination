7f033b17daa35897ea48e76bb85fadea2750f05b
Tesselay Avari <domee.lahl@gmail.com>
2020-11-03 18:28:27 +0100

    Replace raw prototyping with functions

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+def to_one(matrix):
+    multiplicator = 1 / matrix[0][0]
+    for element in range(len(matrix[0])):
+        matrix[0][element] *= multiplicator
+
+    return matrix
+
+
+def nullify_column(matrix, row_num, col_num):
+    nullifier = matrix[row_num][col_num]
+    for element in range(len(matrix[row_num])):
+        matrix[row_num][element] = matrix[row_num][element] - nullifier * matrix[col_num][element]
+
+    return matrix
+
+
+def sign_reverse(matrix, row_num):
+    sign_reverse = -1
+    for element in range(len(matrix[row_num])):
+        matrix[row_num][element] = matrix[row_num][element] * sign_reverse
+
+    return matrix
+
+
 if __name__ == "__main__":
 
     # EXAMPLE MATRIX
@@ -11,43 +35,17 @@ if __name__ == "__main__":
         [1, 3, 4, -2]
     ]
 
-    multiplicator = 1 / matrix[0][0]
-    for element in range(len(matrix[0])):
-        matrix[0][element] *= multiplicator
+    matrix = to_one(matrix)
 
-    for element in range(len(matrix[1])):
-        matrix[1][element] = matrix[1][element] - 1 * matrix[0][element]
+    matrix = nullify_column(matrix, 1, 0)
+    matrix = nullify_column(matrix, 2, 0)
 
-    for element in range(len(matrix[2])):
-        matrix[2][element] = matrix[2][element] - 1 * matrix[0][element]
+    matrix = nullify_column(matrix, 0, 1)
+    matrix = nullify_column(matrix, 2, 1)
 
+    matrix = sign_reverse(matrix, 2)
 
-    for element in range(len(matrix[0])):
-        matrix[0][element] = matrix[0][element] - 1 * matrix[1][element]
-
-    for element in range(len(matrix[2])):
-        matrix[2][element] = matrix[2][element] - 2 * matrix[1][element]
-
-
-    for element in range(len(matrix[2])):
-        matrix[2][element] = matrix[2][element] * -1
-
-
-    for element in range(len(matrix[0])):
-        matrix[0][element] = matrix[0][element] - -1 * matrix[2][element]
-
-    for element in range(len(matrix[1])):
-        matrix[1][element] = matrix[1][element] - 2 * matrix[2][element]
-
-
-
-
-    # multiplicator = 1 / matrix[1][1]
-    # for element in range(len(matrix[1])):
-    #     matrix[1][element] *= multiplicator
-    #
-    # multiplicator = 1 / matrix[2][2]
-    # for element in range(len(matrix[2])):
-    #     matrix[2][element] *= multiplicator
+    matrix = nullify_column(matrix, 0, 2)
+    matrix = nullify_column(matrix, 1, 2)
 
     print(matrix)
