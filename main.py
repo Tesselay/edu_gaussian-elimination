0f8602d7ad4879308186c14ecbb0e3730e107fe9
Tesselay Avari <domee.lahl@gmail.com>
2020-11-03 21:51:43 +0100

    WIP fix algorithm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def to_one(matrix):
-    multiplicator = 1 / matrix[0][0]
-    for element in range(len(matrix[0])):
-        matrix[0][element] *= multiplicator
+def to_one(matrix, row_num, col_num):
+    multiplicator = 1 / matrix[row_num][col_num]
+    for element in range(len(matrix[row_num])):
+        matrix[row_num][element] *= multiplicator
 
     return matrix
 
@@ -28,6 +28,17 @@ def swap_elements(list, pos_1, pos_2):
     return list
 
 
+def correct_pos(matrix, row_num):
+    correct_row = None
+    for element in range(len(matrix)):
+        if matrix[element][row_num] == 1:
+            correct_row = element
+            break
+    if correct_row is not None:
+        swap_elements(matrix, row_num, correct_row)
+
+    return matrix
+
 
 if __name__ == "__main__":
 
@@ -38,47 +49,28 @@ if __name__ == "__main__":
 
     matrix = [
         [1, 1, 1, 3],
-        [1, 2, 3, 0],
+        [2, 2, 3, 0],
         [1, 3, 4, -2]
     ]
 
-    correct_row = None
-    for element in range(len(matrix)):
-        if matrix[element][0] == 1:
-            correct_row = element
-            break
-    if correct_row is not None:
-        swap_elements(matrix, 0, correct_row)
-
-    correct_row = None
-    for element in range(len(matrix)):
-        if matrix[element][1] == 1:
-            correct_row = element
-            break
-    if correct_row is not None:
-        swap_elements(matrix, 1, correct_row)
-
-    correct_row = None
-    for element in range(len(matrix)):
-        if matrix[element][2] == 1:
-            correct_row = element
-            break
-    if correct_row is not None:
-        swap_elements(matrix, 2, correct_row)
+    # matrix = correct_pos(matrix, 0)
+    # matrix = correct_pos(matrix, 1)
+    # matrix = correct_pos(matrix, 2)
 
     print(matrix)
 
-    matrix = to_one(matrix)
+    matrix = to_one(matrix, 0, 0)
 
-    matrix = nullify_column(matrix, 1, 0)
-    matrix = nullify_column(matrix, 2, 0)
-
-    matrix = nullify_column(matrix, 0, 1)
-    matrix = nullify_column(matrix, 2, 1)
-
-    matrix = sign_reverse(matrix, 2)
-
-    matrix = nullify_column(matrix, 0, 2)
-    matrix = nullify_column(matrix, 1, 2)
+    for element in range(len(matrix)):
+        if matrix[element][element] == -1:
+            for sub_element in range(len(matrix[element])):
+                matrix[element][sub_element] *= -1
+        if matrix[element][element] != 1:
+            matrix = to_one(matrix, element, element)
+        try:
+            matrix = nullify_column(matrix, element+1, element)
+        except IndexError as e:
+            matrix = nullify_column(matrix, 0, element)
+        matrix = nullify_column(matrix, element-1, element)
 
     print(matrix)
